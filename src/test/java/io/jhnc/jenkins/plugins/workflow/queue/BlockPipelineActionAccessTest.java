f5c793d8f5c513146f4758ff58f2fc914c3f27b9
offa <bm-dev@yandex.com>
2022-01-11 09:20:02 +0100

    Migrate to JUnit 5 support of Test Harness (#40)

--- src/test/java/io/jhnc/jenkins/plugins/workflow/queue/BlockPipelineActionAccessTest.java
+++ src/test/java/io/jhnc/jenkins/plugins/workflow/queue/BlockPipelineActionAccessTest.java
@@ -26,7 +26,6 @@ package io.jhnc.jenkins.plugins.workflow.queue;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import hudson.scm.NullSCM;
-import io.jhnc.jenkins.plugins.workflow.JenkinsJUnitAdapter;
 import jenkins.branch.Branch;
 import jenkins.branch.MultiBranchProject;
 import jenkins.scm.api.SCMHead;
@@ -35,7 +34,8 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.EnableJenkins;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -44,17 +44,17 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Tag("IT")
-@ExtendWith(JenkinsJUnitAdapter.JenkinsParameterResolver.class)
+@EnableJenkins
 public class BlockPipelineActionAccessTest {
     @Test
-    void blockAndUnblockFailWithoutPost(JenkinsJUnitAdapter.JUnitJenkinsRule r) throws IOException {
+    void blockAndUnblockFailWithoutPost(JenkinsRule r) throws IOException {
         final MultiBranchProject<WorkflowJob, WorkflowRun> project = createMultiBranch(r, "project-0");
         assertFailsWithoutPost(r, project.getUrl() + "block/block");
         assertFailsWithoutPost(r, project.getUrl() + "block/unblock");
     }
 
     @Test
-    void blockAndUnblockOfJobFailWithoutPost(JenkinsJUnitAdapter.JUnitJenkinsRule r) throws IOException {
+    void blockAndUnblockOfJobFailWithoutPost(JenkinsRule r) throws IOException {
         final MultiBranchProject<WorkflowJob, WorkflowRun> project = createMultiBranch(r, "project-1");
         final Branch branch = new Branch("ignored", new SCMHead("master"), new NullSCM(), Collections.emptyList());
         project.getProjectFactory().newInstance(branch);
@@ -62,12 +62,12 @@ public class BlockPipelineActionAccessTest {
         assertFailsWithoutPost(r, project.getUrl() + "block/unblockJob?job=master");
     }
 
-    private WorkflowMultiBranchProject createMultiBranch(JenkinsJUnitAdapter.JUnitJenkinsRule j, String name) throws IOException {
+    private WorkflowMultiBranchProject createMultiBranch(JenkinsRule j, String name) throws IOException {
         return j.jenkins.createProject(WorkflowMultiBranchProject.class, name);
     }
 
-    private void assertFailsWithoutPost(JenkinsJUnitAdapter.JUnitJenkinsRule r, String url) {
-        final JenkinsJUnitAdapter.JUnitJenkinsRule.WebClient webClient = r.createWebClient();
+    private void assertFailsWithoutPost(JenkinsRule r, String url) {
+        final JenkinsRule.WebClient webClient = r.createWebClient();
         final FailingHttpStatusCodeException e = assertThrows(FailingHttpStatusCodeException.class, () -> webClient.goTo(url));
         assertThat(e.getStatusCode()).isEqualTo(405);
     }
